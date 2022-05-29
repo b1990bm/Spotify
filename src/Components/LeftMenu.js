32d2d6ebb56e180b570a722111023f5b933e564e
Behnam <b1990bm@gmail.com>
2022-05-29 18:34:34 +0430

    leftbar logo search

--- src/Components/LeftMenu.js
+++ src/Components/LeftMenu.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import "../Styles/LeftMenu.css";
-
+import { FaSpotify , FaEllipsisH } from "react-icons/fa";
+import { BiSearchAlt } from "react-icons/bi";
 const LeftMenu = () => {
     return (
         <div>
             <div className="leftmenu">
-                
+                <div className="logoContainer">
+                    <i><FaSpotify /></i>
+                    <h2>Spotify</h2>
+                    <i><FaEllipsisH /></i>
+                </div>
+                <div className="searchBax">
+                   <input type="text" placeholder='Search...' />
+                   <i className="serachIcon"><BiSearchAlt /></i> 
+                </div>
             </div>
         </div>
     );
