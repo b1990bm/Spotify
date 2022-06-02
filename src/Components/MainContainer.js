30e43139d0021d0e4fd335cda2a4f077a19175f5
Behnam <b1990bm@gmail.com>
2022-06-02 19:37:59 +0430

    Menu list

--- src/Components/MainContainer.js
+++ src/Components/MainContainer.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React,{ useEffect } from 'react';
+import { FaUsers } from 'react-icons/fa';
 import "../Styles/MainContainer.css";
 import Banner from './Banner';
 
 const MainContainer = () => {
+    useEffect(() => {
+        const allLi=document
+          .querySelector(".menuList")
+          .querySelectorAll("li");
+    
+          function changeMenuActive(){
+            allLi.forEach((n)=>n.classList.remove("active"))
+            this.classList.add("active")
+          }
+    
+          allLi.forEach((n)=>n.addEventListener('click', changeMenuActive))
+      },[])
     return (
         <div className='mainContainer'>
            <Banner /> 
+           <div  className='menuList'>
+               <ul>
+                   <li><a href='#'>Popular</a></li>
+                   <li><a href='#'>Albums</a></li>
+                   <li><a href='#'>Songs</a></li>
+                   <li><a href='#'>Fans</a></li>
+                   <li><a href='#'>About</a></li>
+               </ul>
+               <p><i><FaUsers /></i> 12.3M <span>Follwers</span> </p>
+           </div>
         </div>
     );
 };
